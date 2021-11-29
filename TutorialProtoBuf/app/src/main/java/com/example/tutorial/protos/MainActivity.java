a8ca0de8c1cf386984237232b4ee641cfaf38b9a
JencharFung <jenchar@yeah.net>
2021-11-29 11:35:08 +0800

    更改TutorialProtoBuf，TutorialProtoBufOnAOSP项目名的TutorialProtoBuf，TutorialProtoBufOnAOSP

---
+++ TutorialProtoBuf/app/src/main/java/com/example/tutorial/protos/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tutorial.protos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Person john =
+                Person.newBuilder()
+                        .setId(1234)
+                        .setName("John Doe")
+                        .setEmail("jdoe@example.com")
+                        .addPhones(
+                                Person.PhoneNumber.newBuilder()
+                                        .setNumber("555-4321")
+                                        .setType(Person.PhoneType.HOME))
+                        .build();
+        AddressBook.Builder addressBookBuilder = AddressBook.newBuilder();
+        addressBookBuilder.addPeople(john);
+        AddressBook addressBook = addressBookBuilder.build();
+        Log.d("test", addressBook.toString());
+    }
+}
